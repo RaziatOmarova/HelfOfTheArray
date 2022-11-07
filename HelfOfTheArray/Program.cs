9e64ab2d77d19b9e9734e7520838ea9fef341788
Raziat <114012022+RaziatOmarova@users.noreply.github.com>
2022-11-07 22:49:27 +0300

    Добавьте файлы проекта.

---
+++ HelfOfTheArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelfOfTheArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = new int[8] { 5, 2, 3, 9, 1, 6, 7, 8 };
+            Console.WriteLine(Quantity(number));
+            Console.ReadKey();
+        }
+        static int Quantity(int[] arr)
+        {
+            int temp = arr[0];
+            for (int i = 0; i < arr.Length/2; i++)
+            {
+                if(temp > arr[i])
+                {
+                    temp = arr[i];
+                    arr[i] = i;
+                }
+            }
+            return temp;
+        }
+    }
+}
